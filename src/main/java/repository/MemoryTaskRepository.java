f4e67ca2a1142a8d92324dd27ae984f8e3cae54d
dornesk <dornesk@icloud.com>
2025-07-31 16:29:57 +0300

    Refactored TaskRepository, Implement MemoryTaskRepository with full logic

--- src/main/java/repository/MemoryTaskRepository.java
+++ src/main/java/repository/MemoryTaskRepository.java
@@ -2,32 +2,64 @@ package repository;
 
 import model.Task;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MemoryTaskRepository implements TaskRepository {
     // Хранилище задач: ключ — ID задачи, значение — сама задача
     private final Map<Integer, Task> tasks = new HashMap<>();
 
-    public boolean addTask(Task task) {
-        return false;
+    public void addTask(Task task) {
+        validateTask(task);
+        if (tasks.containsKey(task.getId())) {
+            throw new IllegalArgumentException("Задача с таким ID уже существует");
+        }
+        tasks.put(task.getId(), task);
     }
 
     public Task getTaskById(int id) {
-        return null;
+        validateId(id);
+        ensureTaskExists(id);
+        return tasks.get(id);
     }
 
-    public boolean updateTask(Task task) {
-        return false;
+    public void updateTask(Task task) {
+        validateTask(task);
+        ensureTaskExists(task.getId());
+
+        tasks.replace(task.getId(), task);
     }
 
-    public boolean deleteTask(int id){
-        return false;
+    public void deleteTask(int id) {
+        validateId(id);
+        ensureTaskExists(id);
+        tasks.remove(id);
     }
 
     public List<Task> getAllTasks() {
         return new ArrayList<>(tasks.values());//возврат копии значений, не трогая мапу
+    }
+
+    //валидация поступающих данных task
+    private void validateTask(Task task) {
+        if (task == null) {
+            throw new IllegalArgumentException("Task не может быть null");
+        }
+        if (task.getTitle().isBlank() || task.getId() <= 0) {
+            throw new IllegalArgumentException("Заданы некорректные данные");
+        }
+    }
+
+    //валидация id
+    private void validateId(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Значение ID долэно быть больше нуля");
+        }
+    }
+
+    //проверка наличия объекта в хранилище
+    private void ensureTaskExists(int id) {
+        if (!tasks.containsKey(id)) {
+            throw new NoSuchElementException("Задача с таким ID не найдена");
+        }
     }
 }
--- src/main/java/repository/TaskRepository.java
+++ src/main/java/repository/TaskRepository.java
@@ -5,9 +5,9 @@ import model.Task;
 import java.util.List;
 
 public interface TaskRepository {
-    boolean addTask(Task task);
+    void addTask(Task task);
     Task getTaskById(int id);
-    boolean updateTask(Task task);
-    boolean deleteTask(int id);
+    void updateTask(Task task);
+    void deleteTask(int id);
     List<Task> getAllTasks();
 }
